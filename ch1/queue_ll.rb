1dc07d9c52cfb933f5dbf0ebfcc0276c0c25d598
Harold Nguyen <hanguyen@proofpoint.com>
2015-10-06 12:53:22 -0700

    Finished implementation of queue linked lists

--- ch1/queue_ll.rb
+++ ch1/queue_ll.rb
@@ -2,31 +2,42 @@ require './node'
 
 class QueueLinkedList
 
-	attr_reader :last, :first
+	attr_reader :length
 
 	def initialize(item)
 		@first = Node.new item
 		@first.next = nil
 		@current = @first
+		#-- changes to @current will affect @first, 
+		#   because they are references to the same object
 		@length = 1
+
 	end
 
 	def enqueue item
 		@length += 1
 
+		#-- Get the past node
+		oldnode = @current
+
+		#-- Create the new node
 		newnode = Node.new(item)
-		@first.next = newnode
 
-		newnode.next = @current
+		#-- Point the old node to the new node
+		oldnode.next = newnode
+
+		#-- Save the current node for next time
 		@current = newnode
-
-		# @first.next = newnode
-
 	end
 
 	def dequeue
-		temp = @current.item
-		@current = @current.next
+		if @length == 0
+			return nil
+		end
+
+		@length -= 1 
+		temp = @first.item
+		@first = @first.next
 		temp
 	end
 
