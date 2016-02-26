9d4de0d2559462cec2384369e6c4345a3e2d76fc
Harold Nguyen <haroldnguyen@Harolds-MacBook-Pro.local>
2016-02-25 19:41:28 -0800

    Working version

--- ch1/queue_ll.rb
+++ ch1/queue_ll.rb
@@ -5,40 +5,47 @@ class QueueLinkedList
 	attr_reader :length
 
 	def initialize(item)
-		@first = Node.new item
-		@first.next = nil
-		@current = @first
-		#-- changes to @current will affect @first, 
-		#   because they are references to the same object
+		@q = Node.new item
+		@q.next = nil
+		@current = @q
+		#-- @current holds the item last queued, to keep track of the end of the queue
+		#-- assign @current to the initial node
 		@length = 1
 
 	end
 
 	def enqueue item
-		@length += 1
 
-		#-- Get the past node
-		oldnode = @current
+		if @q.nil?
+			initialize item
+		else
+			#-- Grab the last node from previous time
+			@last = @current
 
-		#-- Create the new node
-		newnode = Node.new(item)
+			@length += 1
 
-		#-- Point the old node to the new node
-		oldnode.next = newnode
+			#-- Create the new node
+			temp = Node.new(item)
 
-		#-- Save the current node for next time
-		@current = newnode
+			#-- Point the old node to the new node
+			@last.next = temp
+
+			#-- Save the current node for next time - reassigning @current now
+			@current = temp
+		end
 	end
 
 	def dequeue
 		if @length == 0
 			return nil
 		end
+			@length -= 1 
 
-		@length -= 1 
-		temp = @first.item
-		@first = @first.next
-		temp
+			#-- We are attempting to peel off from the first
+			first = @q.item
+			@q = @q.next
+			first
+
 	end
 
 end
